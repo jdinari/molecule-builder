83e85e54e0cadfe7010253ccaee54d8ffdfbc6ec
Jasmine Dinari <247475648+jdinari@users.noreply.github.com>
2026-05-20 15:51:03 -0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+setup(
+    name="molbuilder",
+    version="1.0.0",
+    packages=find_packages(),
+    install_requires=["numpy", "scipy"],
+    entry_points={
+        "console_scripts": [
+            "molbuilder=molbuilder.cli:main",
+            "molbuilder-ext=molbuilder.cli_extended:main_extended",
+        ]
+    },
+)
