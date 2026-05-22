6160b508211f45cfbcf111b3cb8fcd793f55cf00
Jasmine Dinari <247475648+jdinari@users.noreply.github.com>
2026-05-22 11:25:54 -0700

    Delete setup.py

--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup, find_packages
-setup(
-    name="molbuilder",
-    version="2.0.0",
-    packages=find_packages(),
-    install_requires=["numpy", "scipy", "rdkit"],
-    entry_points={"console_scripts": ["molbuilder=molbuilder.cli:main"]},
-)
